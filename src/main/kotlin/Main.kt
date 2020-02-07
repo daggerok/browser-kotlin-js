93e3f67dc7b6c62118ab79ee35a5efdf58752983
Maksim Kostromin <daggerok@gmail.com>
2020-02-07 04:45:46 +0200

    Using Kotlinx HTML DSL.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,34 @@
+import kotlinx.html.button
+import kotlinx.html.dom.create
+import kotlinx.html.js.button
+import kotlinx.html.js.div
+import kotlinx.html.js.onClickFunction
+import kotlinx.html.onClick
+import kotlinx.html.p
 import kotlin.browser.document
 import kotlin.browser.window
 
 fun main() {
+    val div = document.create.div {
+        p {
+            +"Okay..."
+        }
+        p {
+            + "Open dev tools and click the button!"
+        }
+        button {
+            +"Click me..."
+            onClickFunction = {
+                println("Who-hoo! ${it.type}")
+            }
+        }
+    }
     document.addEventListener("DOMContentLoaded", {
         console.log("Are you ready for a good jam?")
         document.querySelector("#app")?.let {
             window.setTimeout({
                 it.textContent = "Хола! Их бин Максимко O.o"
+                it.appendChild(div)
             }, 1234)
         }
     }, false)
